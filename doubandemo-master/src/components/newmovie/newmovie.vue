0d82ddf5e718891968dff6bf2fa550574437218f
YangLuan <m17600635332@163.com>
2017-12-23 17:18:52 +0800

    vue2_doubanMaster

---
+++ doubandemo-master/src/components/newmovie/newmovie.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>newMovie</h1>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ doubandemo-master/src/components/test/test.vue
@@ -0,0 +1,10 @@
+<template>
+<h1>test!!!!!</h1>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
